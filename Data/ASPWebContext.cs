2ee85dc5491f086f03fee980538bb9d837ca102f
TimLiu1987 <abc900822@gmail.com>
2024-02-17 15:22:43 +0800

    push up

---
+++ Data/ASPWebContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ASPWeb.Models;
+
+namespace ASPWeb.Data
+{
+    public class ASPWebContext : DbContext
+    {
+        public ASPWebContext (DbContextOptions<ASPWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ASPWeb.Models.Movie> Movie { get; set; } = default!;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,25 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ASPWeb.Data;
+using ASPWeb.Models;
+//建立具有預設值的 WebApplicationBuilder，將 Razor Pages 支援新增至相依性插入 (DI) 容器，並建置應用程式
+var builder = WebApplication.CreateBuilder(args); 
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+//建立具有預設值的 WebApplicationBuilder，將 Razor Pages 支援新增至相依性插入 (DI) 容器，並建置應用程式
+builder.Services.AddDbContext<ASPWebContext>(options =>
+//建立具有預設值的 WebApplicationBuilder，將 Razor Pages 支援新增至相依性插入 (DI) 容器，並建置應用程式
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ASPWebContext") ?? throw new InvalidOperationException("Connection string 'ASPWebContext' not found.")));
 
-var app = builder.Build();
+var app = builder.Build();  //到此為止
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -13,13 +29,13 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+app.UseHttpsRedirection();  //將 HTTP 要求重新導向至 HTTPS
+app.UseStaticFiles();   //允許提供靜態檔案，例如 HTML、CSS、影像和 JavaScript。
 
-app.UseRouting();
+app.UseRouting();   //將路由比對新增至中介軟體管線
 
-app.UseAuthorization();
+app.UseAuthorization(); //授權給使用者存取安全資源
 
-app.MapRazorPages();
+app.MapRazorPages();    //設定 Razor Pages 的端點路由
 
 app.Run();
